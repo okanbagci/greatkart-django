adb201ad65b50e657c0b1f5129e64652e0761875
okanbagci <Ob871903>
2021-08-15 18:03:15 +0300

    store and carts feature added.

--- store/views.py
+++ store/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 from carts.views import _cart_id, CartItem
-# from django.http import HttpResponse
+from django.db.models import Q
+
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -51,3 +53,16 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)

5095f05212fe0971cf7532cbed8a759ea413d1a0
okanbagci <Ob871903>
2021-08-19 19:06:58 +0300

    django authentication added for login, logout, registration.

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,17 @@ STATICFILES_DIRS = [
 # media file configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bagciok@gmail.com'
+EMAIL_HOST_PASSWORD = 'Gmail.871903'
+EMAIL_USE_TLS = True
